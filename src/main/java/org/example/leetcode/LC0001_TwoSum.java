07c13240734579e7d361d732e38aad5e5fec9e26
Juan Ruben Nolasco Silva <jrnolasco@gmail.com>
2024-09-15 18:35:39 -0600

    LC0001_TwoSum adding notes.

--- src/main/java/org/example/leetcode/LC0001_TwoSum.java
+++ src/main/java/org/example/leetcode/LC0001_TwoSum.java
@@ -13,18 +13,31 @@ public class LC0001_TwoSum {
 
     private static final int[] _NUMS = {2, 7, 11, 15};
 
+    /*
+        Find two numbers in the nums array that add up to the target value.
+
+        The code first calculates the complement of the current number (curr) by subtracting it from the target. It
+        then checks if the complement exists in the complements map.
+
+        If it does not exist, the current number and its index are added to the complements map.
+
+        If the complement exists, it means that a pair of numbers that add up to the target has been found, and the
+        method returns an array containing the indices of those numbers.
+     */
+
     public static int[] twoSum(int[] nums, int target) {
         /* Key = complement of current number, Value = index of current number
              i.e. with complements = { 2: 0, 7: 1, 11: 2, 15: 3 } and target = 9, then output = [0, 1]
          */
-        Map<Integer, Integer> complements = new HashMap<>();
+        Map<Integer, Integer> complementsMap = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
             int curr = nums[i];
-            int diff = target - curr;   // calc complement
-            Integer found = complements.get(diff);
+            int complement = target - curr;   // calc complement
+            Integer found = complementsMap.get(complement);
+
             if (found == null)
-                complements.put(curr, i);
+                complementsMap.put(curr, i);
             else
                 return new int[]{found, i};
         }
