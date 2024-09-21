fc70531e96a7eefaf563910bfe62bed722879db6
Juan Ruben Nolasco Silva <jrnolasco@gmail.com>
2024-09-21 16:26:23 -0600

    LC0003_LongestSubstringWithoutRepeatingCharacters and LC0004_MedianOfTwoSortedArrays is not working.

--- src/main/java/org/example/leetcode/LC0003_LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/org/example/leetcode/LC0003_LongestSubstringWithoutRepeatingCharacters.java
@@ -1,15 +1,51 @@
 package org.example.leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
+// https://leetcode.com/problems/longest-substring-without-repeating-characters
+// Explanation: https://youtu.be/fTIzYJvhsqg?si=2QUtcck_8Cu54czD
+// Explanation: https://youtu.be/FCbOzdHKW18?si=MWSd24qFwvgcVoLP
+
 public class LC0003_LongestSubstringWithoutRepeatingCharacters {
 
     public int lengthOfLongestSubstring(String s) {
-        return 0;
+        int maxLength = 0;
+
+        Set<Character> set = new HashSet<>();
+        int left = 0;
+        int right = 0;
+
+        // left and right are two pointers which are used to traverse the string
+        // when the character at the right pointer is not in the set increment the right pointer
+        // when the character at the right pointer is in the set then increment the left pointer
+        while (right < s.length()) {
+
+            // if the character at the right pointer is not in the set, add it to the set
+            // and move the right pointer to the right and increment the max length
+            if (!set.contains(s.charAt(right))) {
+                set.add(s.charAt(right));
+                right++;
+                maxLength = Math.max(maxLength, right - left);
+            }
+            else
+            {
+                // if the character at the right pointer is in the set, then the current windows is not valid
+                // so remove the character at the left pointer and move the left pointer to the right
+                // in order to find a new valid window
+                set.remove(s.charAt(right));
+                left++;
+            }
+
+        }
+
+        return maxLength;
     }
 
     public static void main(String[] args) {
-        LC0003_LongestSubstringWithoutRepeatingCharacters app = new LC0003_LongestSubstringWithoutRepeatingCharacters();
-        app.lengthOfLongestSubstring("abcabcbb");
-        app.lengthOfLongestSubstring("bbbbb");
-        app.lengthOfLongestSubstring("pwwkew");
+        LC0003_LongestSubstringWithoutRepeatingCharacters solution = new LC0003_LongestSubstringWithoutRepeatingCharacters();
+        System.out.println(solution.lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(solution.lengthOfLongestSubstring("bbbbb"));
+        System.out.println(solution.lengthOfLongestSubstring("pwwkew"));
     }
 }
